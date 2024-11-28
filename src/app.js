50185195f6af380bef58d2b010be6157a2b8f831
Lucrecia-15 <parodilucrecia@gmail.com>
2024-11-28 10:50:28 +0000

    my first

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let contentCard = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "K",
+    "Q",
+    "J"
+  ];
+  let symbolCard = ["♥", "♠", "♣", "♦"];
+  let color = ["#000", "#ff0000"];
+
+  let RandomCard = Math.floor(Math.random() * contentCard.length);
+  let RandomSymbol = Math.floor(Math.random() * symbolCard.length);
+  let RandomColor = Math.floor(Math.random() * color.length);
+
+  document.getElementById("number").innerHTML = contentCard[RandomCard];
+  document.getElementById("symbol1").innerHTML = symbolCard[RandomSymbol];
+  document.getElementById("symbol2").innerHTML = symbolCard[RandomSymbol];
+  document.getElementById("symbol1").style.color = color[RandomColor];
+  document.getElementById("symbol2").style.color = color[RandomColor];
 };
